288636155f449adac3c5e300e0a195dac7912d8b
splitice <mat999@gmail.com>
2014-06-20 21:37:55 +1000

    dont daemonize p0f

--- testing/run_p0f.sh
+++ testing/run_p0f.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 BPF_EXPR="tcp dst port 80"
 echo "Generated BPF expression: $BPF_EXPR"
-./p0f -s /var/run/p0f.sock -f ../p0f.fp -i lo -d "$BPF_EXPR" 
+./p0f -s /var/run/p0f.sock -f ../p0f.fp -i lo "$BPF_EXPR" & 
